d9fd98d2c8fca5d661112316e35ba7cecb7b11be
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-20 10:19:24 +0400

    Add vuex

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import store from './store'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(store)
+  .mount('#app')
---
+++ vue/src/store/index.js
@@ -0,0 +1,16 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+  state: {
+    user: {
+      data: {name: 'Zura'},
+      token: null
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {}
+})
+
+export default store;
