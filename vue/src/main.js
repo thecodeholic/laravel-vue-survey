eb5decec45bd560a2cd0d9f0eb7c21b2ee99af1d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-20 10:33:09 +0400

    Install router

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import store from './store'
+import router from './router'
 import './index.css'
 import App from './App.vue'
 
 createApp(App)
   .use(store)
+  .use(router)
   .mount('#app')
---
+++ vue/src/router/index.js
@@ -0,0 +1,10 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+const routes = [];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router;
