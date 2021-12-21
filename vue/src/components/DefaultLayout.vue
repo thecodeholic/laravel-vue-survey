7b47c133417462c239a4cfa18315137b7a4d01e6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-21 10:53:34 +0400

    Code formatting in DefaultLayout.vue

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -14,17 +14,28 @@
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+              <img
+                class="h-8 w-8"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt="Workflow"
+              />
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <router-link v-for="item in navigation"
-                             :key="item.name"
-                             :to="item.to"
-                             active-class='bg-gray-900 text-white'
-                             :class="['px-3 py-2 rounded-md text-sm font-medium', this.$route.name !== item.to.name ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : '']">
-                  {{ item.name }}
-                </router-link>
+                <router-link
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :to="item.to"
+                  active-class="bg-gray-900 text-white"
+                  :class="[
+                    this.$route.name === item.to.name
+                      ? ''
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'px-3 py-2 rounded-md text-sm font-medium',
+                  ]"
+                  >{{ item.name }}</router-link
+                >
+                >
               </div>
             </div>
           </div>
@@ -33,17 +44,60 @@
               <!-- Profile dropdown -->
               <Menu as="div" class="ml-3 relative">
                 <div>
-                  <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                  <MenuButton
+                    class="
+                      max-w-xs
+                      bg-gray-800
+                      rounded-full
+                      flex
+                      items-center
+                      text-sm
+                      focus:outline-none
+                      focus:ring-2
+                      focus:ring-offset-2
+                      focus:ring-offset-gray-800
+                      focus:ring-white
+                    "
+                  >
                     <span class="sr-only">Open user menu</span>
-                    <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                    <img
+                      class="h-8 w-8 rounded-full"
+                      :src="user.imageUrl"
+                      alt=""
+                    />
                   </MenuButton>
                 </div>
-                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                  <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <transition
+                  enter-active-class="transition ease-out duration-100"
+                  enter-from-class="transform opacity-0 scale-95"
+                  enter-to-class="transform opacity-100 scale-100"
+                  leave-active-class="transition ease-in duration-75"
+                  leave-from-class="transform opacity-100 scale-100"
+                  leave-to-class="transform opacity-0 scale-95"
+                >
+                  <MenuItems
+                    class="
+                      origin-top-right
+                      absolute
+                      right-0
+                      mt-2
+                      w-48
+                      rounded-md
+                      shadow-lg
+                      py-1
+                      bg-white
+                      ring-1 ring-black ring-opacity-5
+                      focus:outline-none
+                    "
+                  >
                     <MenuItem v-slot="{ active }">
-                      <a @click="logout" :class="['block px-4 py-2 text-sm text-gray-700 cursor-pointer']">
-                        Logout
-                      </a>
+                      <a
+                        @click="logout"
+                        :class="[
+                          'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
+                        ]"
+                        >Sign out</a
+                      >
                     </MenuItem>
                   </MenuItems>
                 </transition>
@@ -52,7 +106,23 @@
           </div>
           <div class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
-            <DisclosureButton class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <DisclosureButton
+              class="
+                bg-gray-800
+                inline-flex
+                items-center
+                justify-center
+                p-2
+                rounded-md
+                text-gray-400
+                hover:text-white hover:bg-gray-700
+                focus:outline-none
+                focus:ring-2
+                focus:ring-offset-2
+                focus:ring-offset-gray-800
+                focus:ring-white
+              "
+            >
               <span class="sr-only">Open main menu</span>
               <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
               <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
@@ -63,15 +133,19 @@
 
       <DisclosurePanel class="md:hidden">
         <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <router-link v-for="item in navigation"
-                       :key="item.name"
-                       :to="item.to"
-                       active-class="bg-gray-900 text-white"
-                       :class="[
-                         'block px-3 py-2 rounded-md text-base font-medium',
-                         this.$route.name !== item.to.name ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : '']">
-            {{ item.name }}
-          </router-link>
+          <router-link
+            v-for="item in navigation"
+            :key="item.name"
+            :to="item.to"
+            active-class="bg-gray-900 text-white"
+            :class="[
+              this.$route.name === item.to.name
+                ? ''
+                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+              'block px-3 py-2 rounded-md text-base font-medium',
+            ]"
+            >{{ item.name }}</router-link
+          >
         </div>
         <div class="pt-4 pb-3 border-t border-gray-700">
           <div class="flex items-center px-5">
@@ -79,16 +153,31 @@
               <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
             </div>
             <div class="ml-3">
-              <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
-              <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
+              <div class="text-base font-medium leading-none text-white">
+                {{ user.name }}
+              </div>
+              <div class="text-sm font-medium leading-none text-gray-400">
+                {{ user.email }}
+              </div>
             </div>
           </div>
           <div class="mt-3 px-2 space-y-1">
-            <DisclosureButton as="a"
-                              @click="logout"
-                              class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 cursor-pointer hover:text-white hover:bg-gray-700">
-              Logout
-            </DisclosureButton>
+            <DisclosureButton
+              as="a"
+              @click="logout"
+              class="
+                block
+                px-3
+                py-2
+                rounded-md
+                text-base
+                font-medium
+                text-gray-400
+                hover:text-white hover:bg-gray-700
+                cursor-pointer
+              "
+              >Sign out</DisclosureButton
+            >
           </div>
         </div>
       </DisclosurePanel>
@@ -99,16 +188,24 @@
 </template>
 
 <script>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
-import {useRouter} from "vue-router";
-import {useStore} from "vuex";
-import {computed} from "vue";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/vue/outline";
+import { useStore } from "vuex";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
 
 const navigation = [
-  { name: 'Dashboard', to: {name: 'Dashboard'}},
-  { name: 'Surveys', to: {name: 'Surveys'} },
-]
+  { name: "Dashboard", to: { name: "Dashboard" } },
+  { name: "surveys", to: { name: "Surveys" } },
+];
 
 export default {
   components: {
@@ -126,18 +223,19 @@ export default {
   setup() {
     const store = useStore();
     const router = useRouter();
-    function logout(){
-      store.commit('logout');
+
+    function logout() {
+      store.commit("logout");
       router.push({
-        name: 'Login'
-      })
+        name: "Login",
+      });
     }
 
     return {
       user: computed(() => store.state.user.data),
       navigation,
-      logout
-    }
+      logout,
+    };
   },
-}
+};
 </script>
