2d9ebe97e9b1513d3b9b7038fa0c5ffc50ee131d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-21 09:59:30 +0400

    Create AuthLayout.vue. cleanup login and register views

---
+++ vue/src/components/AuthLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import { LockClosedIcon } from '@heroicons/vue/solid'
+
+export default {
+  components: {
+    LockClosedIcon,
+  },
+}
+</script>
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,7 @@ import Surveys from '../views/Surveys.vue'
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
 import store from "../store";
 
 const routes = [
@@ -18,15 +19,24 @@ const routes = [
     ]
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register
+      },
+    ]
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
+
 ];
 
 const router = createRouter({
