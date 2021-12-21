583ac7604f44da9235ac4f225bf5a562c77d5762
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-21 10:32:35 +0400

    Improve navbar and implement logout

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -18,17 +18,18 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                <router-link v-for="item in navigation"
+                             :key="item.name"
+                             :to="item.to"
+                             active-class='bg-gray-900 text-white'
+                             :class="['px-3 py-2 rounded-md text-sm font-medium', this.$route.name !== item.to.name ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : '']">
+                  {{ item.name }}
+                </router-link>
               </div>
             </div>
           </div>
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="h-6 w-6" aria-hidden="true" />
-              </button>
-
               <!-- Profile dropdown -->
               <Menu as="div" class="ml-3 relative">
                 <div>
@@ -39,8 +40,10 @@
                 </div>
                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                   <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                      <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                    <MenuItem v-slot="{ active }">
+                      <a @click="logout" :class="['block px-4 py-2 text-sm text-gray-700 cursor-pointer']">
+                        Logout
+                      </a>
                     </MenuItem>
                   </MenuItems>
                 </transition>
@@ -60,7 +63,15 @@
 
       <DisclosurePanel class="md:hidden">
         <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+          <router-link v-for="item in navigation"
+                       :key="item.name"
+                       :to="item.to"
+                       active-class="bg-gray-900 text-white"
+                       :class="[
+                         'block px-3 py-2 rounded-md text-base font-medium',
+                         this.$route.name !== item.to.name ? 'text-gray-300 hover:bg-gray-700 hover:text-white' : '']">
+            {{ item.name }}
+          </router-link>
         </div>
         <div class="pt-4 pb-3 border-t border-gray-700">
           <div class="flex items-center px-5">
@@ -71,13 +82,13 @@
               <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
               <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
             </div>
-            <button type="button" class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
           </div>
           <div class="mt-3 px-2 space-y-1">
-            <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">{{ item.name }}</DisclosureButton>
+            <DisclosureButton as="a"
+                              @click="logout"
+                              class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 cursor-pointer hover:text-white hover:bg-gray-700">
+              Logout
+            </DisclosureButton>
           </div>
         </div>
       </DisclosurePanel>
@@ -90,24 +101,13 @@
 <script>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+import {useRouter} from "vue-router";
+import {useStore} from "vuex";
+import {computed} from "vue";
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Dashboard', to: {name: 'Dashboard'}},
+  { name: 'Surveys', to: {name: 'Surveys'} },
 ]
 
 export default {
@@ -124,10 +124,19 @@ export default {
     XIcon,
   },
   setup() {
+    const store = useStore();
+    const router = useRouter();
+    function logout(){
+      store.commit('logout');
+      router.push({
+        name: 'Login'
+      })
+    }
+
     return {
-      user,
+      user: computed(() => store.state.user.data),
       navigation,
-      userNavigation,
+      logout
     }
   },
 }
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,13 +3,23 @@ import {createStore} from "vuex";
 const store = createStore({
   state: {
     user: {
-      data: {},
-      token: null
+      data: {
+        name: 'Tom Cook',
+        email: 'tom@example.com',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
+      token: 123
     }
   },
   getters: {},
   actions: {},
-  mutations: {},
+  mutations: {
+    logout: state => {
+      state.user.token = null;
+      state.user.data = {};
+    }
+  },
   modules: {}
 })
 
