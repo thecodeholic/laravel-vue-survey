8730790238658869f5bbbb747a39390ffe4cc216
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-23 11:17:36 +0400

    Add SurveyView component

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
-import {createRouter, createWebHistory} from "vue-router";
-import Dashboard from '../views/Dashboard.vue'
-import Surveys from '../views/Surveys.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Surveys from "../views/Surveys.vue";
+import SurveyView from "../views/SurveyView.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
@@ -9,49 +10,52 @@ import store from "../store";
 
 const routes = [
   {
-    path: '/',
-    redirect: '/dashboard',
+    path: "/",
+    redirect: "/dashboard",
     component: DefaultLayout,
-    meta: {requiresAuth: true},
+    meta: { requiresAuth: true },
     children: [
-      {path: '/dashboard', name: 'Dashboard', component: Dashboard},
-      {path: '/surveys', name: 'Surveys', component: Surveys}
-    ]
+      { path: "/dashboard", name: "Dashboard", component: Dashboard },
+      { path: "/surveys", name: "Surveys", component: Surveys },
+      { path: "/survey/:id", name: "SurveyView", component: SurveyView },
+    ],
   },
   {
-    path: '/auth',
-    redirect: '/login',
-    name: 'Auth',
+    path: "/auth",
+    redirect: "/login",
+    name: "Auth",
     component: AuthLayout,
     children: [
       {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: "/login",
+        name: "Login",
+        component: Login,
       },
       {
-        path: '/register',
-        name: 'Register',
-        component: Register
+        path: "/register",
+        name: "Register",
+        component: Register,
       },
-    ]
+    ],
   },
-
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth && !store.state.user.token) {
-    next({name: 'Login'})
-  } else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')) {
-    next({name: 'Dashboard'});
+    next({ name: "Login" });
+  } else if (
+    store.state.user.token &&
+    (to.name === "Login" || to.name === "Register")
+  ) {
+    next({ name: "Dashboard" });
   } else {
-    next()
+    next();
   }
-})
+});
 
 export default router;
---
+++ vue/src/views/SurveyView.vue
@@ -0,0 +1,21 @@
+<template>
+  <PageComponent :title="survey.title">
+    <pre>{{ survey }}</pre>
+  </PageComponent>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import store from "../store";
+import PageComponent from "../components/PageComponent.vue";
+
+const router = useRouter();
+const route = useRoute();
+
+const survey = computed(() =>
+  store.state.surveys.find((s) => s.id === parseInt(route.params.id))
+);
+</script>
+
+<style></style>
