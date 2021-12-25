e5373cea90631e143cf99d0ef626f1667f3bac28
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-25 08:57:12 +0400

    Implement user registration

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,13 +3,8 @@ import { createStore } from "vuex";
 const store = createStore({
   state: {
     user: {
-      data: {
-        name: "Tom Cook",
-        email: "tom@example.com",
-        imageUrl:
-          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-      },
-      token: 123,
+      data: {},
+      token: sessionStorage.getItem('TOKEN'),
     },
     surveys: [
       {
@@ -45,12 +40,33 @@ const store = createStore({
     ],
   },
   getters: {},
-  actions: {},
+  actions: {
+    register({commit}, user) {
+      return fetch(`http://localhost:8000/api/register`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(user)
+      })
+        .then(res => res.json())
+        .then(res => {
+          commit('setUser', res);
+          return res;
+        })
+    }
+  },
   mutations: {
     logout: (state) => {
       state.user.token = null;
       state.user.data = {};
     },
+    setUser: (state, userData) => {
+      state.user.token = userData.token;
+      state.user.data = userData.user;
+      sessionStorage.setItem('TOKEN', userData.token);
+    }
   },
   modules: {},
 });
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -13,26 +13,34 @@
       </router-link>
     </p>
   </div>
-  <form class="mt-8 space-y-6" action="#" method="POST">
+  <form class="mt-8 space-y-6" @submit="register">
     <input type="hidden" name="remember" value="true"/>
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
         <label for="fullname" class="sr-only">Email address</label>
-        <input id="fullname" name="name" type="text" autocomplete="name" required=""
+        <input id="fullname" name="name" type="text" autocomplete="name" required="" v-model="user.name"
                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                placeholder="Full name"/>
       </div>
       <div>
         <label for="email-address" class="sr-only">Email address</label>
-        <input id="email-address" name="email" type="email" autocomplete="email" required=""
+        <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email"
                class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                placeholder="Email address"/>
       </div>
       <div>
         <label for="password" class="sr-only">Password</label>
         <input id="password" name="password" type="password" autocomplete="current-password" required=""
-               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+               v-model="user.password"
+               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                placeholder="Password"/>
+      </div>
+      <div>
+        <label for="password_confirmation" class="sr-only">Password</label>
+        <input id="password_confirmation" name="password_confirmation" type="password" autocomplete="current-password"
+               required="" v-model="user.password_confirmation"
+               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+               placeholder="Confirm Password"/>
       </div>
     </div>
 
@@ -48,12 +56,27 @@
   </form>
 </template>
 
-<script>
+<script setup>
 import {LockClosedIcon} from '@heroicons/vue/solid'
+import store from "../store";
+import {useRouter} from "vue-router";
 
-export default {
-  components: {
-    LockClosedIcon,
-  },
+const router = useRouter();
+const user = {
+  name: '',
+  email: '',
+  password: ''
+};
+
+function register(ev) {
+  ev.preventDefault();
+  store
+    .dispatch('register', user)
+    .then(() => {
+      router.push({
+        name: 'Dashboard'
+      })
+    })
 }
+
 </script>
