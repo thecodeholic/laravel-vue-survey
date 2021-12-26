da406333066c651f00b8c2ccfb9f85c60ea7bf1d
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-26 11:34:33 +0400

    Display survey list nicely with cards

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,25 +1,88 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <PageComponent title="Surveys">
-    <router-link
-      v-for="survey in surveys"
-      :key="survey.id"
-      :to="{ name: 'SurveyView', params: { id: survey.id } }"
-      class="
-        block
-        py-4
-        px-6
-        shadow-md
-        bg-white
-        mb-5
-        rounded-md
-        hover:bg-blue-50
-        cursor-pointer
-      "
-    >
-      <h4 class="text-lg font-bold">{{ survey.title }}</h4>
-      <div v-html="survey.description"></div>
-    </router-link>
+    <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+      <div
+        v-for="survey in surveys"
+        :key="survey.id"
+        class="
+          flex flex-col
+          py-4
+          px-6
+          shadow-md
+          bg-white
+          hover:bg-gray-50
+          h-[470px]
+        "
+      >
+        <img :src="survey.image" alt="" class="w-full h-48 object-cover" />
+        <h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
+        <div v-html="survey.description" class="overflow-hidden flex-1"></div>
+
+        <div class="flex justify-between items-center mt-3">
+          <router-link
+            :to="{ name: 'SurveyView', params: { id: survey.id } }"
+            class="
+              flex
+              py-2
+              px-4
+              border border-transparent
+              text-sm
+              rounded-md
+              text-white
+              bg-indigo-600
+              hover:bg-indigo-700
+              focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+            "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 mr-2"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+              />
+            </svg>
+            Edit
+          </router-link>
+          <a
+            type="submit"
+            class="
+              h-12
+              w-12
+              flex
+              items-center
+              justify-center
+              rounded-full
+              border border-transparent
+              text-sm text-indigo-500
+              focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+            "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
   </PageComponent>
 </template>
 
