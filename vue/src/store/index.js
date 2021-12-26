f2c5443a79cd27bf6a631c944a6c1c13ebe2dc49
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-26 14:58:37 +0400

    Add questions empty array to store state dummy surveys

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -117,6 +117,7 @@ const store = createStore({
         created_at: "2021-12-20 18:00:00",
         updated_at: "2021-12-20 18:00:00",
         expire_date: "2021-12-31 18:00:00",
+        questions: []
       },
       {
         id: 3,
@@ -128,6 +129,7 @@ const store = createStore({
         created_at: "2021-12-21 17:00:00",
         updated_at: "2021-12-21 17:00:00",
         expire_date: "2021-12-31 00:00:00",
+        questions: []
       },
       {
         id: 4,
@@ -139,6 +141,7 @@ const store = createStore({
         created_at: "2021-12-21 14:00:00",
         updated_at: "2021-12-21 14:00:00",
         expire_date: "2021-12-31 00:00:00",
+        questions: []
       },
     ],
     questionTypes: ['text', 'select', 'radio', 'checkbox', 'textarea']
