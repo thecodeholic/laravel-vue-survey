8dc1f7ba75655a06d07bcb69cb0c331ca428fade
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2021-12-26 11:33:32 +0400

    Remove vertical padding on PageController

--- vue/src/components/PageComponent.vue
+++ vue/src/components/PageComponent.vue
@@ -9,7 +9,7 @@
   <main>
     <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">
+      <div class="px-4 sm:px-0">
         <slot></slot>
       </div>
       <!-- /End replace -->
