085c46d9c23f4dab723094a09ca9e3014852fb01
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-05 10:05:55 +0400

    Use `useRouter` in SurveyView.vue instead of importing router

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -307,11 +307,12 @@
 <script setup>
 import { v4 as uuidv4 } from "uuid";
 import { computed, ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import store from "../store";
 import PageComponent from "../components/PageComponent.vue";
 import QuestionEditor from "../components/editor/QuestionEditor.vue";
-import router from "../router";
+
+const router = useRouter()
 
 const route = useRoute();
 
