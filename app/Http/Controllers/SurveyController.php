28deff502e09eb6d954cc8d308805068ab3a0511
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-07 17:34:08 +0400

    Implement image uploading on survey creation

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -37,6 +37,13 @@ class SurveyController extends Controller
     public function store(StoreSurveyRequest $request)
     {
         $data = $request->validated();
+
+        // Check if image was given and save on local file system
+        if (isset($data['image'])) {
+            $relativePath  = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
         $survey = Survey::create($data);
         foreach ($data['questions'] as $question) {
             $question['survey_id'] = $survey->id;
