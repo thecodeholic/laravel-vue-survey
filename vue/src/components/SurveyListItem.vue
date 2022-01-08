ef64607e5118a237b9777f82996f20f4f4d1ad08
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-08 09:07:56 +0400

    Add default image to SurveyListItem.vue when image is null

--- vue/src/components/SurveyListItem.vue
+++ vue/src/components/SurveyListItem.vue
@@ -2,7 +2,14 @@
   <div
     class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]"
   >
-    <img :src="survey.image_url" alt="" class="w-full h-48 object-cover" />
+    <img
+      :src="
+        survey.image_url ||
+        'https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg'
+      "
+      :alt="survey.title"
+      class="w-full h-48 object-cover"
+    />
     <h4 class="mt-4 text-lg font-bold">{{ survey.title }}</h4>
     <div v-html="survey.description" class="overflow-hidden flex-1"></div>
 
@@ -72,10 +79,10 @@
 </template>
 
 <script setup>
-const {survey} = defineProps({
-  survey: Object
-})
-const emit = defineEmits(['delete', 'edit'])
+const { survey } = defineProps({
+  survey: Object,
+});
+const emit = defineEmits(["delete", "edit"]);
 </script>
 
 <style></style>
