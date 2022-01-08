a4532650b489e7bffe450db13fc5a5fc6fc1a192
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-08 09:10:45 +0400

    Update getting surveys
    Implement pagination,
    Show loading on surveys
    Add animation on each survey in the list

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -26,13 +26,44 @@
         </router-link>
       </div>
     </template>
-    <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
-      <SurveyListItem
-        v-for="survey in surveys"
-        :key="survey.id"
-        :survey="survey"
-        @delete="deleteSurvey"
-      />
+    <div v-if="surveys.loading" class="flex justify-center">Loading...</div>
+    <div v-else>
+      <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+        <SurveyListItem
+          v-for="(survey, ind) in surveys.data"
+          :key="survey.id"
+          :survey="survey"
+          class="opacity-0 animate-fade-in-down"
+          :style="{ animationDelay: `${ind * 0.1}s` }"
+          @delete="deleteSurvey"
+        />
+      </div>
+      <div class="flex justify-center mt-5">
+        <nav
+          class="relative z-0 inline-flex justify-center rounded-md shadow-sm -space-x-px"
+          aria-label="Pagination"
+        >
+          <!-- Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" -->
+          <a
+            v-for="(link, i) of surveys.links"
+            :key="i"
+            :disabled="!link.url"
+            href="#"
+            @click="getForPage(link)"
+            aria-current="page"
+            class="relative inline-flex items-center px-4 py-2 border text-sm font-medium whitespace-nowrap"
+            :class="[
+              link.active
+                ? 'z-10 bg-indigo-50 border-indigo-500 text-indigo-600'
+                : 'bg-white border-gray-300 text-gray-500 hover:bg-gray-50',
+              i === 0 ? 'rounded-l-md bg-gray-100 text-gray-700' : '',
+              i === surveys.links.length - 1 ? 'rounded-r-md' : '',
+            ]"
+            v-html="link.label"
+          >
+          </a>
+        </nav>
+      </div>
     </div>
   </PageComponent>
 </template>
@@ -58,5 +89,14 @@ function deleteSurvey(survey) {
   ) {
     store.dispatch("deleteSurvey", survey.id);
   }
+}
+
+function getForPage(link) {
+  console.log(link);
+  if (!link.url || link.active) {
+    return;
+  }
+
+  store.dispatch("getSurveys", { url: link.url });
 }
 </script>
--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          "from": {
+            transform: "translateY(-0.75rem)",
+            opacity: '0'
+          },
+          "to": {
+            transform: "translateY(0rem)",
+            opacity: '1'
+          },
+        },
+      },
+      animation: {
+        'fade-in-down': "fade-in-down 0.2s ease-in-out both",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
