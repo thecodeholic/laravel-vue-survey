f4484cf78ffee60ea6e553b6c415f9429a6d1d93
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-16 14:48:58 +0400

    Make a request to get user info on load.

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -42,11 +42,24 @@
             <div class="ml-4 flex items-center md:ml-6">
               <!-- Profile dropdown -->
               <Menu as="div" class="ml-3 relative">
-                <div>
+                <div class="flex">
                   <MenuButton
                     class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                   >
                     <span class="sr-only">Open user menu</span>
+
+                    <div class="mx-3">
+                      <div
+                        class="text-left text-base font-medium leading-none text-white"
+                      >
+                        {{ user.name }}
+                      </div>
+                      <div
+                        class="text-sm font-medium leading-none text-gray-400"
+                      >
+                        {{ user.email }}
+                      </div>
+                    </div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       class="h-8 w-8"
@@ -74,7 +87,7 @@
                   <MenuItems
                     class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                   >
-                    <MenuItem v-slot="{ active }">
+                    <MenuItem v-slot="{}">
                       <a
                         @click="logout"
                         :class="[
@@ -208,6 +221,8 @@ export default {
         });
       });
     }
+
+    store.dispatch("getUser");
 
     return {
       user: computed(() => store.state.user.data),
