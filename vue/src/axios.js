67bfca50deed0f5617048a7ad4f341845e907101
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-16 09:43:26 +0400

    Use API_BASE_URL from .env

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import store from "./store";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 })
 
 axiosClient.interceptors.request.use(config => {
