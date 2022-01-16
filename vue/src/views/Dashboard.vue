8bce2b7caa3c1d09d163a6021a3397a2ad0d88d1
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-16 14:47:51 +0400

    Fix bug when latest survey does not exist on dashboard

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -11,7 +11,7 @@
       >
         <h3 class="text-2xl font-semibold">Total Surveys</h3>
         <div
-          class="text-8xl font-semibold flex-1 flex items-center justify-center"
+          class="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center"
         >
           {{ data.totalSurveys }}
         </div>
@@ -22,7 +22,7 @@
       >
         <h3 class="text-2xl font-semibold">Total Answers</h3>
         <div
-          class="text-8xl font-semibold flex-1 flex items-center justify-center"
+          class="text-8xl pb-4 font-semibold flex-1 flex items-center justify-center"
         >
           {{ data.totalAnswers }}
         </div>
@@ -31,72 +31,77 @@
         class="row-span-2 animate-fade-in-down order-3 lg:order-1 bg-white shadow-md p-4"
       >
         <h3 class="text-2xl font-semibold">Latest Survey</h3>
-        <img
-          :src="data.latestSurvey.image_url"
-          class="w-[240px] mx-auto"
-          alt=""
-        />
-        <h3 class="font-bold text-xl mb-3">{{ data.latestSurvey.title }}</h3>
-        <div class="flex justify-between text-sm mb-1">
-          <div>Create Date:</div>
-          <div>{{ data.latestSurvey.created_at }}</div>
-        </div>
-        <div class="flex justify-between text-sm mb-1">
-          <div>Expire Date:</div>
-          <div>{{ data.latestSurvey.expire_date }}</div>
-        </div>
-        <div class="flex justify-between text-sm mb-1">
-          <div>Status:</div>
-          <div>{{ data.latestSurvey.status ? 'Active' : 'Draft' }}</div>
-        </div>
-        <div class="flex justify-between text-sm mb-1">
-          <div>Questions:</div>
-          <div>{{ data.latestSurvey.questions }}</div>
-        </div>
-        <div class="flex justify-between text-sm mb-3">
-          <div>Answers:</div>
-          <div>{{ data.latestSurvey.answers }}</div>
-        </div>
-        <div class="flex justify-between">
-          <router-link
-            :to="{ name: 'SurveyView', params: { id: data.latestSurvey.id } }"
-            class="flex py-2 px-4 border border-transparent text-sm rounded-md text-indigo-500 hover:bg-indigo-700 hover:text-white transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+        <div v-if="data.latestSurvey">
+          <img
+            :src="data.latestSurvey.image_url"
+            class="w-[240px] mx-auto"
+            alt=""
+          />
+          <h3 class="font-bold text-xl mb-3">{{ data.latestSurvey.title }}</h3>
+          <div class="flex justify-between text-sm mb-1">
+            <div>Create Date:</div>
+            <div>{{ data.latestSurvey.created_at }}</div>
+          </div>
+          <div class="flex justify-between text-sm mb-1">
+            <div>Expire Date:</div>
+            <div>{{ data.latestSurvey.expire_date }}</div>
+          </div>
+          <div class="flex justify-between text-sm mb-1">
+            <div>Status:</div>
+            <div>{{ data.latestSurvey.status ? "Active" : "Draft" }}</div>
+          </div>
+          <div class="flex justify-between text-sm mb-1">
+            <div>Questions:</div>
+            <div>{{ data.latestSurvey.questions }}</div>
+          </div>
+          <div class="flex justify-between text-sm mb-3">
+            <div>Answers:</div>
+            <div>{{ data.latestSurvey.answers }}</div>
+          </div>
+          <div class="flex justify-between">
+            <router-link
+              :to="{ name: 'SurveyView', params: { id: data.latestSurvey.id } }"
+              class="flex py-2 px-4 border border-transparent text-sm rounded-md text-indigo-500 hover:bg-indigo-700 hover:text-white transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-              />
-            </svg>
-            Edit Survey
-          </router-link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+                />
+              </svg>
+              Edit Survey
+            </router-link>
 
-          <button
-            class="flex py-2 px-4 border border-transparent text-sm rounded-md text-indigo-500 hover:bg-indigo-700 hover:text-white transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5 mr-2"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+            <button
+              class="flex py-2 px-4 border border-transparent text-sm rounded-md text-indigo-500 hover:bg-indigo-700 hover:text-white transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-              <path
-                fill-rule="evenodd"
-                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            View Answers
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 mr-2"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              View Answers
+            </button>
+          </div>
+        </div>
+        <div v-else class="text-gray-600 text-center py-16">
+          Your don't have surveys yet
         </div>
       </div>
       <div
@@ -113,18 +118,24 @@
             View all
           </a>
         </div>
-        <a
-          href="#"
-          v-for="answer of data.latestAnswers"
-          :key="answer.id"
-          class="block p-2 hover:bg-gray-100/90"
-        >
-          <div class="font-semibold">{{ answer.survey.title }}</div>
-          <small>
-            Answer Made at:
-            <i class="font-semibold">{{ answer.end_date }}</i>
-          </small>
-        </a>
+
+        <div v-if="data.latestAnswers.length">
+          <a
+            href="#"
+            v-for="answer of data.latestAnswers"
+            :key="answer.id"
+            class="block p-2 hover:bg-gray-100/90"
+          >
+            <div class="font-semibold">{{ answer.survey.title }}</div>
+            <small>
+              Answer Made at:
+              <i class="font-semibold">{{ answer.end_date }}</i>
+            </small>
+          </a>
+        </div>
+        <div v-else class="text-gray-600 text-center py-16">
+          Your don't have answers yet
+        </div>
       </div>
     </div>
   </PageComponent>
