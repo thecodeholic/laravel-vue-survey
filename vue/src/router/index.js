adf83f4e4f13049e99c1ec211ef363825233edfa
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-16 16:40:58 +0400

    Change routes for surveys/create

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -19,8 +19,8 @@ const routes = [
     children: [
       { path: "/dashboard", name: "Dashboard", component: Dashboard },
       { path: "/surveys", name: "Surveys", component: Surveys },
-      { path: "/survey", name: "SurveyCreate", component: SurveyView },
-      { path: "/survey/:id", name: "SurveyView", component: SurveyView },
+      { path: "/surveys/create", name: "SurveyCreate", component: SurveyView },
+      { path: "/surveys/:id", name: "SurveyView", component: SurveyView },
     ],
   },
   {
