af035b2fc2e891b1de75fd697e530c8568a5af15
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-01-16 14:47:19 +0400

    Create getUser action and update setUser mutation

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -29,23 +29,36 @@ const store = createStore({
   },
   getters: {},
   actions: {
-    register({ commit }, user) {
-      return axiosClient.post("/register", user).then(({ data }) => {
-        commit("setUser", data);
-        return data;
-      });
+
+    register({commit}, user) {
+      return axiosClient.post('/register', user)
+        .then(({data}) => {
+          commit('setUser', data.user);
+          commit('setToken', data.token)
+          return data;
+        })
     },
-    login({ commit }, user) {
-      return axiosClient.post("/login", user).then(({ data }) => {
-        commit("setUser", data);
-        return data;
-      });
+    login({commit}, user) {
+      return axiosClient.post('/login', user)
+        .then(({data}) => {
+          commit('setUser', data.user);
+          commit('setToken', data.token)
+          return data;
+        })
     },
-    logout({ commit }) {
-      return axiosClient.post("/logout").then((response) => {
-        commit("logout");
-        return response;
-      });
+    logout({commit}) {
+      return axiosClient.post('/logout')
+        .then(response => {
+          commit('logout')
+          return response;
+        })
+    },
+    getUser({commit}) {
+      return axiosClient.get('/user')
+      .then(res => {
+        console.log(res);
+        commit('setUser', res.data)
+      })
     },
     getDashboardData({commit}) {
       commit('dashboardLoading', true)
@@ -127,7 +140,7 @@ const store = createStore({
     },
     saveSurveyAnswer({commit}, {surveyId, answers}) {
       return axiosClient.post(`/survey/${surveyId}/answer`, {answers});
-    }
+    },
   },
   mutations: {
     logout: (state) => {
@@ -135,10 +148,13 @@ const store = createStore({
       state.user.data = {};
       sessionStorage.removeItem("TOKEN");
     },
-    setUser: (state, userData) => {
-      state.user.token = userData.token;
-      state.user.data = userData.user;
-      sessionStorage.setItem("TOKEN", userData.token);
+
+    setUser: (state, user) => {
+      state.user.data = user;
+    },
+    setToken: (state, token) => {
+      state.user.token = token;
+      sessionStorage.setItem('TOKEN', token);
     },
     dashboardLoading: (state, loading) => {
       state.dashboard.loading = loading;
